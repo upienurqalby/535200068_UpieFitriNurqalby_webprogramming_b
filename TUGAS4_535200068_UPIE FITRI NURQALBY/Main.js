dad3ade1db242a9ebbdd27233e3879dde4ae3668
upienurqalby <88979545+upienurqalby@users.noreply.github.com>
2021-09-14 05:35:46 +0700

    TUGAS 4

---
+++ TUGAS4_535200068_UPIE FITRI NURQALBY/Main.js
@@ -0,0 +1,21 @@
+function myUniversal() {
+    document.querySelector("*").style.backgroundcolor = "red";
+}
+function myType() {
+    document.querySelector("h1").style.backgroundcolor = "blue";
+}
+function myType() {
+    document.querySelector("h1").style.backgroundcolor = "blue";
+}
+function myClass() {
+    document.querySelector(".container").style.backgroundcolor = "yellow";
+}
+function myId() {
+    document.querySelector("#logo").style.backgroundcolor = "blue";
+}
+function myAttribute() {
+    document.querySelector("a[target]").style.backgroundcolor = "red";
+}
+function myGroup() {
+    document.querySelector("h2, h1").style.backgroundcolor = "red";
+}
